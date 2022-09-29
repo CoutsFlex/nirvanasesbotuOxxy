d3cce8dae054afac664a5065301fa356e2fec138
CoutsFlex <114684605+CoutsFlex@users.noreply.github.com>
2022-09-29 19:18:38 +0300

    Add files via upload

---
+++ Nirvana Ses Bot/ayarlar.js
@@ -0,0 +1,21 @@
+module.exports = {
+  discow: {
+    footer: "Couts Regal Was Here",
+    type: "LISTENING",
+    status: "dnd",
+    sahipler: ["763490351205187645"],
+    yetkirol: "1023282276588601486",
+    sunucuid: "1023282276383064104"
+  },
+  odalar: {
+    welcome1: "1023282277435854904", 
+    welcome2: "1023282277435854905",
+    welcome3: "1023282277435854906",
+    welcome4: "1023282277435854907",
+    welcome5: "1023282277435854908"
+  }, 
+  sesler: {
+    yetkili: "https://cdn.glitch.com/c5569289-4eb7-4b9a-b36b-8e4df90a6c8a%2Findir_3.mp3?v=1627836077506",
+    hg: "https://cdn.glitch.com/c5569289-4eb7-4b9a-b36b-8e4df90a6c8a%2Findir_2%20(1).mp3?v=1627836079718"
+  }
+}
